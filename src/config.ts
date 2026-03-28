daaa7b425274eb57bfe52f72c1c354e79228bc14
hanbin chen <hanbin.9797@gmail.com>
2026-03-28 04:37:59 +0100

    fix: update site title, author, and description in theme config

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
     website: 'https://chiri.the3ash.com/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
+    title: 'Hanbin Chen', // Site title
+    author: 'Hanbin Chen', // Author name
+    description: "Hanbin Chen's blog", // Site description
     language: 'en-US' // Default language
   },
 
